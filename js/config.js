a9a2945c53160da10d33684b4ba16268ccbc88e3
Eshika <150813030+Eshika-Ranjan-Singh@users.noreply.github.com>
2025-09-24 21:56:38 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_uaCitie4p', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'nlgka7uue6tnamn9odhvtp2vc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_tg624Snhg', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3gshpie1hi0nhu5lj663abbrin', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://rax6dp3xj1.execute-api.ap-south-1.amazonaws.com/dev' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
